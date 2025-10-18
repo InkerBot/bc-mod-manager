cf33ceb5cba5da748003eb2338b54dc453bb0ad6
InkerBot <im@inker.bot>
2025-10-19 03:13:52 +0800

    allow use esc key to close modal

--- src/component/ModalContainer.tsx
+++ src/component/ModalContainer.tsx
@@ -12,6 +12,7 @@ interface ModalContainerState {
  */
 export default class ModalContainer extends Component<{}, ModalContainerState> {
   private unsubscribe?: () => void;
+  private escKeyListener?: (event: KeyboardEvent) => void;
 
   constructor(props: {}) {
     super(props);
@@ -25,12 +26,25 @@ export default class ModalContainer extends Component<{}, ModalContainerState> {
     this.unsubscribe = ModalService.subscribe((modals) => {
       this.setState({modals});
     });
+
+    this.escKeyListener = (event: KeyboardEvent) => {
+      if (event.key === 'Escape' && this.state.modals.length > 0) {
+        const topmostModal = this.state.modals[this.state.modals.length - 1];
+        this.handleModalClose(topmostModal.id, 'close');
+        event.preventDefault();
+      }
+    };
+    document.addEventListener('keydown', this.escKeyListener);
   }
 
   componentWillUnmount() {
     // Unsubscribe when component unmounts
     if (this.unsubscribe) {
       this.unsubscribe();
+    }
+
+    if (this.escKeyListener) {
+      document.removeEventListener('keydown', this.escKeyListener);
     }
   }
 
